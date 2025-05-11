5425f16cfff3150eefb10b22cfe2a7f3cfb384e5
root <root@haru-2024>
2025-05-11 14:42:51 +0900

    top_page

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,9 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  get "how_to_use", to: "pages#how_to_use"
+  get "trial", to: "pages#trial"
+
   # ルートパス
   root "posts#index"
   # お問い合わせページへのルート
